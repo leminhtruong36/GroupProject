e3d06ef0002c59e2c4b2e62045d55eb9a2082916
ttDi <131509450+trieudi17063004@users.noreply.github.com>
2024-07-06 10:10:45 +0700

    Add files via upload

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,9 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using GroupProject.Models;
-using X.PagedList;
 using Microsoft.EntityFrameworkCore;
+using X.PagedList;
+using GroupProject.ViewModels;
 namespace GroupProject.Controllers;
 
 public class HomeController : Controller
@@ -14,27 +15,42 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-     public IActionResult Index(int? page)
-   {
+    public IActionResult Index(int? page)
+    {
 
-     int pageSize = 8;
-     int pageNumber = page == null || page < 0 ? 1 : page.Value;
-     var lstsanpham = db.DanhMucSPs.AsNoTracking().OrderBy(x => x.TenSP);
-     PagedList<DanhMucSP> lst = new PagedList<DanhMucSP>(lstsanpham, pageNumber, pageSize);
-     return View(lstsanpham);
-   }
+        int pageSize = 8;
+        int pageNumber = page == null || page < 0 ? 1 : page.Value;
+        var lstsanpham = db.DanhMucSPs.AsNoTracking().OrderBy(x => x.TenSP);
+        PagedList<DanhMucSP> lst = new PagedList<DanhMucSP>(lstsanpham, pageNumber, pageSize);
+        return View(lstsanpham);
+    }
 
-    public IActionResult SanPhamTheoLoai(int maloai, int? page)
-  {
-//Doi String maloai thanh int maloai
-     int pageSize = 8;
-     int pageNumber = page == null || page < 0 ? 1 : page.Value;
-     var lstsanpham = db.DanhMucSPs.AsNoTracking().Where(x=>x.MaLoai=maloai)
-         .OrderBy(x => x.TenSP);
-     PagedList<DanhMucSP> lst = new PagedList<DanhMucSP>(lstsanpham, pageNumber, pageSize);
-     ViewBag.maloai = maloai;
-     return View(lstsanpham);
-  }
+    public IActionResult SanPhamTheoLoai(string maloai, int? page)
+    {
+        int pageSize = 8;
+        int pageNumber = page == null || page < 0 ? 1 : page.Value;
+        var lstsanpham = db.DanhMucSPs.AsNoTracking()
+            .OrderBy(x => x.TenSP);
+        PagedList<DanhMucSP> lst = new PagedList<DanhMucSP>(lstsanpham, pageNumber, pageSize);
+        ViewBag.maloai = maloai;
+        return View(lst);
+    }
+    public IActionResult ChiTietSanPham(int maSp)
+    {
+        var sanPham = db.DanhMucSPs.SingleOrDefault(x => x.MaSP == maSp);
+        var anhSanPham= db.AnhSPs.Where(x=>x.MaSP == maSp).ToList();
+        ViewBag.anhSanPham = anhSanPham;
+        return View(sanPham);
+    }
+    public IActionResult ProductDetal(int maSp)
+    {
+        var sanPham = db.DanhMucSPs.SingleOrDefault(x => x.MaSP == maSp);
+        var anhSanPham = db.AnhSPs.Where(x => x.MaSP == maSp).ToList();
+        var homeProductDetailViewModel = new HomeProductDetailViewModels{
+            danhMucSP = sanPham,
+            anhSps=anhSanPham };
+        return View(homeProductDetailViewModel);
+    }
     public IActionResult Privacy()
     {
         return View();
---
+++ ViewModels/HomeProductDetailViewModels.cs
@@ -0,0 +1,9 @@
+﻿using GroupProject.Models;
+namespace GroupProject.ViewModels
+{
+    public class HomeProductDetailViewModels
+    {
+        public DanhMucSP danhMucSP { get; set; }
+        public List<AnhSp> anhSps { get; set; }
+    }
+}
